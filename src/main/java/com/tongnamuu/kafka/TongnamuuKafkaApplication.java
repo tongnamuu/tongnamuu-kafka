98fd9c8adda575416d856990d60096896265fadd
tongnamuu <thban33@naver.com>
2024-08-12 11:29:57 +0900

    project init

---
+++ src/main/java/com/tongnamuu/kafka/TongnamuuKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.tongnamuu.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongnamuuKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TongnamuuKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tongnamuu/kafka/TongnamuuKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongnamuu.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongnamuuKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
